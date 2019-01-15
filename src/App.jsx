193d2c069b8dffd536f14e1e98e0b1b84e5afaab
dhanyn10 <dhanyn.dhan@gmail.com>
2019-01-15 19:31:09 +0700

    test page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import BrowserRouter from 'react-router-dom';
+
+class App extends Component
+{
+    render()
+    {
+        return (
+            <div>
+                hello from app
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import 'babel-preset-env';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
